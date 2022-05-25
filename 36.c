2acebd6813ecfec6e90ed49c83cbe2f98a49af07
Santiago-Calvelo <102254693+Santiago-Calvelo@users.noreply.github.com>
2022-05-25 01:52:26 -0300

    Update 36.c

--- 36.c
+++ 36.c
@@ -6,12 +6,10 @@ int main(int argc, char const *argv[]) {
 
   int sum[] = {v[0] + u[0], v[1] + u[1]};
   int res[] = {v[0] - u[0], v[1] - u[1]};
-  int prodv[] = {5 * v[0], 5 * v[1]};
-  int produ[] = {5 * u[0], 5 * u[1]};
+  int prod = v[0] * u[0] + v[1] * u[1];
 
   printf("Suma= (%d;%d)\n", sum[0], sum[1]);
   printf("Resta= (%d;%d)\n", res[0], res[1]);
-  printf("Producto escalar(v)= (%d;%d)\n", prodv[0], prodv[1]);
-  printf("Producto escalar(u)= (%d;%d)\n", produ[0], produ[1]);
+  printf("Producto escalar= %d\n", prod);
 return 0;
 }
